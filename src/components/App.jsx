2d15447ea111b48badec9e93694af7b4eb52f06f
Bruno Vinícius <mail@bruno.vin>
2018-07-15 16:49:48 -0300

    add favicons, styles adjusts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import format from 'date-fns/format'
-import { Button, Container } from 'reactstrap'
+import { Button, Container, Alert } from 'reactstrap'
 import Places from './Places/Places'
 
 class App extends Component {
@@ -15,16 +15,13 @@ class App extends Component {
 			latitude: undefined,
 			error: false,
 			code: undefined,
-			responseFull: undefined,
-			venues: undefined,
-			venueId: undefined,
-			placeImages: undefined
+			venues: undefined
 		}
 	}
 
 	places = () => {
-		const CLIENT_ID = 'OCDVFW22BQFU4H0JXZW4ILB2KFHOYP2GIYV04QJDBGVC1IGX'
-		const CLIENT_SECRET = 'Y5WQPMK4YCZ4JP2SQ4UU0PWUSU22VUL2D4LCPI530V0VW2VH'
+		const CLIENT_ID = 'T2LPSOLZ5QN313AVWZIOFYMTENZFECV3I2H33V0Q435ANRED'
+		const CLIENT_SECRET = 'RZTUNDKJ12MBH1GR30YPHJU3RCRLR5VT20ELPYTQJ1XH3HUL'
 		const date = format(new Date(), 'YYYYMMDD')
 		// fetch places
 		fetch(`https://api.foursquare.com/v2/venues/search?ll=${this.state.latitude},${this.state.longitude}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${date}`)
@@ -33,21 +30,9 @@ class App extends Component {
 				console.log(res.response.venues)
 				this.setState({
 					code: res.meta.code,
-					responseFull: res,
-					venues: res.response.venues,
-					venueId: res.response.venues.id
+					venues: res.response.venues
 				})
 			})
-
-		// // fetch places images
-		// fetch(`https://api.foursquare.com/v2/venues/4efcc946f9ab0847fa5fcced/photos?&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${date}`)
-		// 	.then(res => res.json())
-		// 	.then(res => {
-		// 		console.log(res.response.photos.items)
-		// 		this.setState({
-		// 			placeImages: res.response.photos.items
-		// 		})
-		// 	})
 	}
 
 	getLocation = () => {
@@ -68,26 +53,6 @@ class App extends Component {
 		})
 	}
 
-	loadImages = (id) => {
-		const CLIENT_ID = 'OCDVFW22BQFU4H0JXZW4ILB2KFHOYP2GIYV04QJDBGVC1IGX'
-		const CLIENT_SECRET = 'Y5WQPMK4YCZ4JP2SQ4UU0PWUSU22VUL2D4LCPI530V0VW2VH'
-		const date = format(new Date(), 'YYYYMMDD')
-
-		fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${date}`)
-			.then(res => res.json())
-			.then(res => {
-				if (res.response.photos !== undefined) {
-					this.setState({
-						placeImages: res.response.photos.items
-					})
-					console.log(this.state.placeImages)
-					return this.state.placeImages
-				} else {
-					console.log('sem imagens para exibir')
-				}
-			})
-	}
-
 	render() {
 		const dataReceived = this.state.venues
 		return (
@@ -101,11 +66,21 @@ class App extends Component {
 						<Places
 							code={this.state.code}
 							dataReceived={dataReceived}
-							responseFull={this.state.responseFull}
-							placeImages={this.state.placeImages}
-							loadImages={() => this.loadImages()} />
+						/>
 					</Container>
 					: null
+				}
+
+				{
+					this.state.code === 400 &&
+					<Alert color='danger' style={{ textAlign: 'center' }}>
+						Houve um erro ao buscar os dados. Tente novamente mais tarde.
+					</Alert>
+				}
+
+				{
+					this.state.code === 403 &&
+					<h2>Cota excedida. Tente novamente amanhã.</h2>
 				}
 
 				{this.state.error === true &&
--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -1,34 +1,22 @@
 import React from 'react'
 
-const Places = ({ dataReceived, placeImages, loadImages }) => (
+const Places = ({ dataReceived }) => (
 	<div>
 		{
 			dataReceived.map((item, key) =>
 				<div className='Places' key={key}>
+
+					<img
+						className='icon-categories'
+						alt={item.categories[0].name}
+						src={`${item.categories[0].icon.prefix}88.png`}
+					/>
+
 					<p>Place: {item.name}</p>
 
 					<p>Place ID: {item.id}</p>
 
 					<p>Categoria: {item.categories[0].name}</p>
-
-					<p><img
-						className='icon-categories'
-						alt={item.categories[0].name}
-						src={`${item.categories[0].icon.prefix}88.png`} />
-					</p>
-					{/* {() => loadImages('4efcc946f9ab0847fa5fcced')} */}
-					{placeImages !== undefined ?
-						placeImages.map((Image, key) =>
-							<div key={key}>
-								<img
-									alt='teste'
-									src={`https://igx.4sqi.net/img/general/300x500${Image.suffix}`}
-								/>
-							</div>
-						) : null
-					}
-					<button onClick={() => loadImages('4efcc946f9ab0847fa5fcced')}>Chamar imagens</button>
-					<p>Place Image: </p>
 
 					<p>Estado: {item.location.state}</p>
 
